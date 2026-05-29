cb4a9e52de8ba2a9ae13409564f5c19b70205e04
Priya Chauhan <priyaschauhan0607@gmail.com>
2026-05-29 17:03:15 +0530

    Added edit and delete student features

--- student_management/students/urls.py
+++ student_management/students/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('delete/<int:id>/', views.delete_student, name='delete_student'),
+    path('edit/<int:id>/', views.edit_student, name='edit_student'),
 ]
--- student_management/students/views.py
+++ student_management/students/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student
+
 
 def home(request):
     students = Student.objects.all()
@@ -7,3 +8,26 @@ def home(request):
     return render(request, 'home.html', {
         'students': students
     })
+
+
+def delete_student(request, id):
+    student = get_object_or_404(Student, id=id)
+    student.delete()
+
+    return redirect('home')
+
+
+def edit_student(request, id):
+    student = get_object_or_404(Student, id=id)
+
+    if request.method == 'POST':
+        student.name = request.POST['name']
+        student.roll_no = request.POST['roll_no']
+        student.course = request.POST['course']
+        student.email = request.POST['email']
+        student.phone = request.POST['phone']
+
+        student.save()
+        return redirect('home')
+
+    return render(request, 'edit.html', {'student': student})
