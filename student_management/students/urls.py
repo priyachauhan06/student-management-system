f0f15b5b86292546488c6fa9896616abba771aaa
Priya Chauhan <priyaschauhan0607@gmail.com>
2026-05-30 15:48:33 +0530

    Restructured project hierarchy

--- student_management/students/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('delete/<int:id>/', views.delete_student, name='delete_student'),
-    path('edit/<int:id>/', views.edit_student, name='edit_student'),
-]
