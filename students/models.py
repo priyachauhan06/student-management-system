00cf421220f9904734b4e38325c0dd44ff60bcc9
Priya Chauhan <priyaschauhan0607@gmail.com>
2026-05-30 13:01:29 +0530

    Project files

---
+++ students/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Student(models.Model):
+
+    name = models.CharField(max_length=100)
+    roll_no = models.IntegerField()
+    course = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
